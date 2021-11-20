eecccc6776a042077a26a45c4096cd2adac7aa08
LufyCZ <lufy@sushi.com>
2021-11-20 21:37:50 +0000

    Initial commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,113 +1,63 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address, log } from "@graphprotocol/graph-ts";
 import {
-  BentoBox,
-  LogDeploy,
   LogDeposit,
-  LogFlashLoan,
-  LogRegisterProtocol,
-  LogSetMasterContractApproval,
-  LogStrategyDivest,
-  LogStrategyInvest,
-  LogStrategyLoss,
-  LogStrategyProfit,
-  LogStrategyQueued,
-  LogStrategySet,
-  LogStrategyTargetPercentage,
-  LogTransfer,
-  LogWhiteListMasterContract,
   LogWithdraw,
-  OwnershipTransferred
-} from "../generated/BentoBox/BentoBox"
-import { ExampleEntity } from "../generated/schema"
+  LogFlashLoan,
+  LogStrategyProfit,
+  LogStrategyLoss,
+} from "../generated/BentoBox/BentoBox";
+import { Token } from "../generated/schema";
+import { BIG_INT_ZERO } from "./constants";
 
-export function handleLogDeploy(event: LogDeploy): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function getToken(address: Address): Token {
+  let token = Token.load(address.toHex());
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  if (token === null) {
+    token = new Token(address.toHex());
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    token.totalSupplyBase = BIG_INT_ZERO;
+    token.totalSupplyElastic = BIG_INT_ZERO;
+
+    token.save();
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.masterContract = event.params.masterContract
-  entity.data = event.params.data
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DOMAIN_SEPARATOR(...)
-  // - contract.balanceOf(...)
-  // - contract.masterContractApproved(...)
-  // - contract.masterContractOf(...)
-  // - contract.nonces(...)
-  // - contract.owner(...)
-  // - contract.pendingOwner(...)
-  // - contract.pendingStrategy(...)
-  // - contract.strategy(...)
-  // - contract.strategyData(...)
-  // - contract.toAmount(...)
-  // - contract.toShare(...)
-  // - contract.totals(...)
-  // - contract.whitelistedMasterContracts(...)
-  // - contract.withdraw(...)
+  return token as Token;
 }
 
-export function handleLogDeposit(event: LogDeposit): void {}
+export function handleLogDeposit(event: LogDeposit): void {
+  const token = getToken(event.params.token);
+  token.totalSupplyBase = token.totalSupplyBase.plus(event.params.share);
+  token.totalSupplyElastic = token.totalSupplyElastic.plus(event.params.amount);
+  token.save();
+}
 
-export function handleLogFlashLoan(event: LogFlashLoan): void {}
+export function handleLogWithdraw(event: LogWithdraw): void {
+  const token = getToken(event.params.token);
+  token.totalSupplyBase = token.totalSupplyBase.minus(event.params.share);
+  token.totalSupplyElastic = token.totalSupplyElastic.minus(
+    event.params.amount
+  );
+  token.save();
+}
 
-export function handleLogRegisterProtocol(event: LogRegisterProtocol): void {}
+export function handleLogStrategyProfit(event: LogStrategyProfit): void {
+  const token = getToken(event.params.token);
+  token.totalSupplyElastic = token.totalSupplyElastic.plus(event.params.amount);
+  token.save();
+}
 
-export function handleLogSetMasterContractApproval(
-  event: LogSetMasterContractApproval
-): void {}
+export function handleLogStrategyLoss(event: LogStrategyLoss): void {
+  const token = getToken(event.params.token);
+  token.totalSupplyElastic = token.totalSupplyElastic.minus(
+    event.params.amount
+  );
+  token.save();
+}
 
-export function handleLogStrategyDivest(event: LogStrategyDivest): void {}
-
-export function handleLogStrategyInvest(event: LogStrategyInvest): void {}
-
-export function handleLogStrategyLoss(event: LogStrategyLoss): void {}
-
-export function handleLogStrategyProfit(event: LogStrategyProfit): void {}
-
-export function handleLogStrategyQueued(event: LogStrategyQueued): void {}
-
-export function handleLogStrategySet(event: LogStrategySet): void {}
-
-export function handleLogStrategyTargetPercentage(
-  event: LogStrategyTargetPercentage
-): void {}
-
-export function handleLogTransfer(event: LogTransfer): void {}
-
-export function handleLogWhiteListMasterContract(
-  event: LogWhiteListMasterContract
-): void {}
-
-export function handleLogWithdraw(event: LogWithdraw): void {}
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+export function handleLogFlashLoan(event: LogFlashLoan): void {
+  const token = getToken(event.params.token);
+  token.totalSupplyElastic = token.totalSupplyElastic.plus(
+    event.params.feeAmount
+  );
+  token.save();
+}
